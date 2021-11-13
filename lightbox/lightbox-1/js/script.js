419ef23072dd580738ce818b0a56245801361242
rogerdavid2 <rogerdavidb57@gmail.com>
2021-11-12 20:05:00 -0800

    Add files via upload

--- lightbox/lightbox-1/js/script.js
+++ lightbox/lightbox-1/js/script.js
@@ -4,14 +4,10 @@ function close() {
     overlay.classList.remove('active');
 }
 
-// The divs are buttons
 const divButtons = document.querySelectorAll('.project');
-/* For each div button add an event listener 
-that when clicked will grab the image src
-and assign it to the IMG tag inside them html */
 divButtons.forEach(button => {
     button.addEventListener("click", () => {
-        const imageSource = button.children[0].src; /* Clicked image src */
+        const imageSource = button.children[0].src;
         gImages.src = imageSource; /* Assign clicked img src to IMG html src */
         overlay.classList.add('active');
     })
